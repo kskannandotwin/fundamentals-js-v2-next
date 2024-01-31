7234542954af5f6a0e91c7c2fa7d8f44186c1740
kskannandotwin <kskannan.win@gmail.com>
2024-01-31 18:11:42 +0530

    31.01.2024-functions
    completed

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-"use strict";
+function logger() {
+  console.log("My name is Jonas");
+}
 
-let hasDriversLicense = false;
-const passTest = true;
+// calling / running / invoking function
+logger();
+logger();
+logger();
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
 
-// const interface = 'Audio';
-// const private = 534;
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
