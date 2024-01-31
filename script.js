d4415452a8a1e2d8270794f8d24bcae81c159075
kskannandotwin <kskannan.win@gmail.com>
2024-01-31 18:14:15 +0530

    31.01.2024-fdve
    completed

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-function logger() {
-  console.log("My name is Jonas");
+// Function declaration
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
 }
+const age1 = calcAge1(1991);
 
-// calling / running / invoking function
-logger();
-logger();
-logger();
+// Function expression
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+const age2 = calcAge2(1991);
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-const num = Number("23");
+console.log(age1, age2);
