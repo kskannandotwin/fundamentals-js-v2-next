b88788159ff5d2610eb1a2fb57da14f771424f86
kskannandotwin <kskannan.win@gmail.com>
2024-01-31 18:15:52 +0530

    31.01.2024-af
    completed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Function declaration
-function calcAge1(birthYeah) {
-  return 2037 - birthYeah;
-}
-const age1 = calcAge1(1991);
+const calcAge3 = (birthYeah) => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
 
-// Function expression
-const calcAge2 = function (birthYeah) {
-  return 2037 - birthYeah;
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
 };
-const age2 = calcAge2(1991);
 
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
