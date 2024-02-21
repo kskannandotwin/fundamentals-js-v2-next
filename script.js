11a2207731094877ede001cce9209ac99c77113d
kannanATexcelforte <kannan@excelforte.com>
2024-02-21 14:53:32 +0530

    21.02.2024-itfl-fix
    completed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// console.log('Lifting weights repetition 1');
-// console.log('Lifting weights repetition 2');
-// console.log('Lifting weights repetition 3');
-// console.log('Lifting weights repetition 4');
-// console.log('Lifting weights repetition 5');
-// console.log('Lifting weights repetition 6');
-// console.log('Lifting weights repetition 7');
-// console.log('Lifting weights repetition 8');
-// console.log('Lifting weights repetition 9');
-// console.log('Lifting weights repetition 10');
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
 
 // for loop keeps running while condition is TRUE
 for (let rep = 1; rep <= 30; rep++) {
-  console.log(`Lifting weights repetition ${rep}`);
+  console.log(`Lifting weights repetition ${rep}`);
 }
