544f38bb725ab50ad86745c5287e0ebd76368f27
kannanATexcelforte <kannan@excelforte.com>
2024-02-22 11:31:52 +0530

    22.02.2024-twl-fix
    completed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights repetition ${rep} ðŸ‹ï¸â€â™€ï¸`);
+  console.log(`Lifting weights repetition ${rep}.`);
 }
 
 let rep = 1;
 while (rep <= 10) {
-  // console.log(`WHILE: Lifting weights repetition ${rep} ðŸ‹ï¸â€â™€ï¸`);
+  // console.log(`WHILE: Lifting weights repetition ${rep}.`);
   rep++;
 }
 
